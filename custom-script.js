8b4a57e2bde82362be269682a1f48a443f18cff1
Yelshyn Andrii <ben.limitedvision@gmail.com>
2023-02-09 16:59:21 +0200

    hidden flights

---
+++ custom-script.js
@@ -0,0 +1,14 @@
+const addFlightBlock = document.querySelector('.add-flight-block');
+const hiddenBlock = document.querySelector('.flight-details');
+const arrow = document.querySelector('.arrow');
+
+addFlightBlock.addEventListener('click', (e) => {
+    e.preventDefault();
+    hiddenBlock.classList.toggle('hidden');
+    if (hiddenBlock.classList.contains('hidden')) {
+        arrow.innerHTML = "▼";
+    }
+    else {
+        arrow.innerHTML = "▲";
+    }
+})
