bf5b0966d365a32c33f8aa3e977cc10a30654ee8
Yelshyn Andrii <ben.limitedvision@gmail.com>
2023-02-09 17:38:29 +0200

    underlined hidden

--- custom-script.js
+++ custom-script.js
@@ -1,15 +1,19 @@
 const addFlightBlock = document.querySelector('.add-flight-block');
 const hiddenBlock = document.querySelector('.flight-details');
 const arrow = document.querySelector('.arrow');
+const flightTitle = document.querySelector('.flight-title');
 
 addFlightBlock.addEventListener('click', (e) => {
     e.preventDefault();
     hiddenBlock.classList.toggle('hidden');
 
     if (hiddenBlock.classList.contains('hidden')) {
-        arrow.innerHTML = "▼";
+        arrow.innerHTML = "▼";  
+        flightTitle.style.textDecoration = 'underline 2px #30D5C8';
+
     }
     else {
         arrow.innerHTML = "▲";
+        flightTitle.style.textDecoration = 'none';
     }
 })
